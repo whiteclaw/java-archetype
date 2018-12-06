f0970ee8f74115f7128081242c84bb9499849bbb
whiteclaw <whiteclaw@yaoshi.local>
2018-12-06 22:46:04 +0800

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,11 @@
+package ${groupId};
+
+/**
+ * @author ${USER}
+ * created on ${DATE}
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,26 @@
+package ${groupId};
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ${USER}
+ * created on ${DATE}
+ */
+public class AppTest {
+    @BeforeAll
+    public void init(){
+
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Test");
+    }
+
+    @AfterAll
+    public void close(){
+
+    }
+}
